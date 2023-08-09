83708201d155d2e6fe366d9330e65d46ca6c3892
Joe Polny <joepolny@gmail.com>
2023-08-09 09:05:18 -0400

    working big box

--- contracts/algo-did.algo.ts
+++ contracts/algo-did.algo.ts
@@ -1,25 +1,105 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Contract } from '@algorandfoundation/tealscript';
 
+/*
+start - The index of the box at which the data starts
+end - The index of the box at which the data ends
+status - 0: in progress, 1: ready, 2: immutable
+endSize - The size of the last box
+*/
+type Metadata = {start: uint64, end: uint64, status: uint<8>, endSize: uint64};
+
+const IN_PROGRESS = 0 as uint<8>;
+const READY = 1 as uint<8>;
+const IMMUTABLE = 2 as uint<8>;
+
+const COST_PER_BYTE = 400;
+const COST_PER_BOX = 2500;
+const MAX_BOX_SIZE = 32768;
+
 // eslint-disable-next-line no-unused-vars
-class AlgoDid extends Contract {
-  private getSum(a: number, b: number): number {
-    return a + b;
+class AlgoDID extends Contract {
+  // The boxes that contain the data, indexed by uint64
+  dataBoxes = new BoxMap<uint64, bytes>();
+
+  // Metadata for a given data identifier
+  // The data identifier can be any string up to 64 bytes
+  metadata = new BoxMap<bytes, Metadata>();
+
+  // The index of the next box to be created
+  currentIndex = new GlobalStateKey<uint64>();
+
+  /**
+   *
+   * Allocate boxes to begin data upload process
+   *
+   * @param dataIdentifier The unique identifier for the data
+   * @param numBoxes The number of boxes that the data will take up
+   * @param endBoxSize The size of the last box
+   * @param mbrPayment Payment from the uploader to cover the box MBR
+   */
+  startUpload(
+    dataIdentifier: string,
+    numBoxes: uint64,
+    endBoxSize: uint64,
+    mbrPayment: PayTxn,
+  ): void {
+    const startBox = this.currentIndex.get();
+    const endBox = startBox + numBoxes - 1;
+
+    const metadata: Metadata = {
+      start: startBox, end: endBox, status: IN_PROGRESS, endSize: endBoxSize,
+    };
+
+    assert(!this.metadata.exists(dataIdentifier));
+
+    this.metadata.set(dataIdentifier, metadata);
+
+    this.currentIndex.set(endBox + 1);
+
+    const totalCost = numBoxes * COST_PER_BOX // cost of boxes
+    + (numBoxes - 1) * MAX_BOX_SIZE * COST_PER_BYTE // cost of data
+    + numBoxes * 64 * COST_PER_BYTE // cost of keys
+    + endBoxSize * COST_PER_BYTE; // cost of last box data
+
+    assert(mbrPayment.amount === totalCost);
+    assert(mbrPayment.receiver === this.app.address);
   }
 
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
+  /**
+   *
+   * Upload data to a specific offset in a box
+   *
+   * @param dataIdentifier The unique identifier for the data
+   * @param boxIndex The index of the box to upload the given chunk of data to
+   * @param offset The offset within the box to start writing the data
+   * @param data The data to write
+   */
+  upload(dataIdentifier: string, boxIndex: uint64, offset: uint64, data: bytes): void {
+    const metadata = this.metadata.get(dataIdentifier);
+    assert(metadata.status === IN_PROGRESS);
+    assert(metadata.start <= boxIndex && boxIndex <= metadata.end);
+
+    if (offset === 0) {
+      this.dataBoxes.create(boxIndex, boxIndex === metadata.end ? metadata.endSize : MAX_BOX_SIZE);
+    }
+
+    this.dataBoxes.replace(boxIndex, offset, data);
   }
 
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
+  /**
+   *
+   * Set the status of the data
+   *
+   * @param dataIdentifier The unique identifier for the data
+   * @param status The new status for the data
+   */
+  setStatus(dataIdentifier: string, status: uint<8>): void {
+    const currentStatus = this.metadata.get(dataIdentifier).status;
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
+    assert(status === READY || status === IMMUTABLE || status === IN_PROGRESS);
+    assert(currentStatus !== IMMUTABLE);
 
-    return result;
+    this.metadata.get(dataIdentifier).status = status;
   }
 }
